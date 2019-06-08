b222275a2b5f54e02dfba920663df48dbd6f7c4e
medeuov92 <47469578+medeuov92@users.noreply.github.com>
2019-06-08 18:36:14 +0600

    Add files via upload

---
+++ tree.py
@@ -0,0 +1,6 @@
+my_tree = ['h', ['i', ['t',['o'],['m']], ['h',['i'],['t']] ], ['t',
+['o',['m'],['!']], []] ]
+print(my_tree)
+print('left subtree = ', my_tree[1])
+print('root = ', my_tree[0])
+print('right subtree = ', my_tree[2])
